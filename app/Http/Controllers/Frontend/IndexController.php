a2d70fde11b077b874bcf0f72ff24d5acc76b690
asmfahim <abusmfahimf9@gmail.com>
2022-12-03 02:09:23 +0600

    Forntend category and slider

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Slider;
 
 class IndexController extends Controller
 {
     public function index(){
-        return view('Frontend.layouts.index');
+        $sliders = Slider::where('status',1)->orderBy('id','DESC')->limit(3)->get();
+        $categoris = Category::orderBy('category_name_en','ASC')->get();
+        return view('Frontend.layouts.index',compact('categoris','sliders'));
     }
 }
